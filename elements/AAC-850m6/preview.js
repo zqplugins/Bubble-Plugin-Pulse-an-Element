57d0bbad4885b10e20235a33b067a768973c702e
zqplugins <137153217+zqplugins@users.noreply.github.com>
2023-11-14 20:17:13 +0300

    Automatic commit from Bubble

--- elements/AAC-850m6/preview.js
+++ elements/AAC-850m6/preview.js
@@ -1,5 +1,21 @@
 function(instance, properties) {
-
-
-
+	const imgSrc = "//meta-l.cdn.bubble.io/f1697302087809x110766138459000620/gear.svg";
+    
+    const img = $(`<img src=${imgSrc} />`)
+    img.css({
+        "position": "absolute",
+    	"width": "100%",
+        "height": "100%",
+    })
+    
+    const preview = $(`<div></div>`).append(img);
+    preview.css({
+        "box-sizing": "border-box",
+        "width": "100%",
+        "height": "100%",
+        "overflow": "hidden",
+        "opacity": "50%",
+    });
+    
+    instance.canvas.append(preview);
 }
--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -1,3 +1,7 @@
 function(instance, properties, context) {
-let elements=properties.element_id.split(",");for(var i=0;i<elements.length;i++)$("#"+elements[i]).pulse({opacity:[0,1]},{times:999999});
+    if(instance.data.secondsTime) return;
+    instance.data.secondsTime = true;
+    let times = Number(properties.times === "infinity" ? "Infinity" : properties.times);
+    if(isNaN(times)) {times = "Infinity"; context.reportDebugger("Invalid parameter 'Times': " + properties.times +", instead will be used: 'Infinity'. Please check")}
+const elements=properties.element_id.split(",");for(var i=0;i<elements.length;i++)$("#"+elements[i]).pulse({opacity:[0,1]},{times, duration: properties.duration});
 }
